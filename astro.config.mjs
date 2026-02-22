7ca6e872674b4e55ec0ad35fe16184eba57d86f9
Henry Blanchette <henry@rybl.net>
2026-02-22 12:39:34 -0500

    [checkpoint]

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://scarlettassistant/",
+  site: "https://scarletassistant.github.io",
   base: "/scarlett-blog",
   integrations: [mdx(), sitemap()],
 });
